9db048c49f5d0b72aacff77c6cf68f5783480b2d
Nelson Segarra <junicus@outlook.com>
2016-03-07 20:13:46 -0400

    linters for the win, fixed my jscsrc file and fixed the spacing on my src filers

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-var fs = require('fs')
-var express = require('express')
-var multer = require('multer')
-var upload = multer({dest: 'uploadFiles/'})
-var app = express()
+var fs = require('fs');
+var express = require('express');
+var multer = require('multer');
+var upload = multer({ dest: 'uploadFiles/' });
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('pages/index')
-})
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
 
-app.post('/api/file', upload.array('filesToAnalyze', 3), function (req, res, next) {
-  console.log('api/file called')
-  console.log(req.files)
-  var data = {
-    files: []
-  }
+app.post('/api/file', upload.array('filesToAnalyze', 3), function(req, res, next) {
+    console.log('api/file called');
+    console.log(req.files);
+    var data = {
+        files: []
+    };
 
-  req.files.forEach(function (file) {
-    data.files.push(
-      {
-        originalname: file.originalname,
-        size: file.size,
-        mimetype: file.mimetype
-      }
-    )
+    req.files.forEach(function(file) {
+        data.files.push(
+            {
+                originalname: file.originalname,
+                size: file.size,
+                mimetype: file.mimetype
+            }
+        );
 
-    var filePath = __dirname + '/' + file.path
-    console.log('Removing: ' + filePath)
-    fs.unlinkSync(filePath)
-  })
+        var filePath = __dirname + '/' + file.path;
+        console.log('Removing: ' + filePath);
+        fs.unlinkSync(filePath);
+    })
 
-  res.json(data)
-})
+    res.json(data);
+});
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'))
-})
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
-  $('form').on('submit', function (event) {
-    event.stopPropagation()
-    event.preventDefault()
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-    var data = new FormData(this)
-    $.ajax({
-      url: window.location.origin + '/api/file',
-      type: 'POST',
-      data: data,
-      cache: false,
-      processData: false,
-      contentType: false,
-      error: function (jqXHR, textStatus, errorThrown) {
-        $('.erropanel').toggleClass('collapsed')
-        $('.errormsg').html(textStatus)
-      },
-      success: function (data) {
-        $('.metapanel').toggleClass('collapsed')
-        // $('.jsondata').html(JSON.stringify(data))
-        data['files'].forEach(function (elem) {
-          var filename = $('<span />', {text: 'Filename: ' + elem.originalname})
-          var filesize = $('<span />', {text: 'Filesize: ' + elem.size})
-          var item = $('<li />')
-          item.append(filename)
-          item.append(', ')
-          item.append(filesize)
-          $('.filelist').append(item)
-        })
-      }
-    })
-  })
-})
+        var data = new FormData(this);
+        $.ajax({
+            url: window.location.origin + '/api/file',
+            type: 'POST',
+            data: data,
+            cache: false,
+            processData: false,
+            contentType: false,
+            error: function(jqXHR, textStatus, errorThrown) {
+                $('.erropanel').toggleClass('collapsed');
+                $('.errormsg').html(textStatus);
+            },
+            success: function(data) {
+                $('.metapanel').toggleClass('collapsed');
+                // $('.jsondata').html(JSON.stringify(data))
+                data['files'].forEach(function(elem) {
+                    var filename = $('<span />', { text: 'Filename: ' + elem.originalname });
+                    var filesize = $('<span />', { text: 'Filesize: ' + elem.size });
+                    var item = $('<li />');
+                    item.append(filename);
+                    item.append(', ');
+                    item.append(filesize);
+                    $('.filelist').append(item);
+                });
+            }
+        });
+    });
+});
